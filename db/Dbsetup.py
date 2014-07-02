6269000ce46e0f7a190fd4c6bf02f84baf1d1a87
jakesyl <jakesyl@gmail.com>
2014-07-02 18:46:05 -0400

    readability fix

--- db/Dbsetup.py
+++ db/Dbsetup.py
@@ -1,8 +1,11 @@
+'''
+Creates database and subsequent tables on users computer
+'''
 import sqlite3
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.db')#intalizing db
 c = conn.cursor()
 #setting up all of the tables
-#clusters
+#cluster creation
 c.execute('''CREATE TABLE clusters(id real, contents real)''')
 #files
 c.execute('''CREATE TABLE fileMap(path text, cluster real, size text)''')
