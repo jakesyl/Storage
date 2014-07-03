7f51038a46c18539cff2a4adc16f2be75aeb59e3
jakesyl <jakesyl@gmail.com>
2014-07-02 22:30:33 -0400

    added new code that shall be deleted

--- src/index.py
+++ src/index.py
@@ -2,16 +2,15 @@
 Jake Sylvestre
 
 '''
-'''#test code
-print "This is using getsize to see how much every file consumes"
-print "---------------"
-from os.path import join, getsize
-for root, dirs, files in os.walk('~'):
-    print root, "consumes",
-    print sum([getsize(join(root, name)) for name in files]),
-    print "bytes in", len(files), "non-directory files"
+import os
 
-'''
+print os.path.expanduser('~')
+
+for root, dirs, files in os.walk(os.path.expanduser('~')):
+	print root, dirs, files
+	print " "
+
+
 ''' 
 Sudocode:
 Using os.walk design a for loop that starts at the root and crawls through every file 
@@ -20,10 +19,26 @@ Using os.walk design a for loop that starts at the root and crawls through every
 
 More Details:
 Dir: ~/cortexstorage/src
-'''
+
+
+Other code:
+1:
+
 import os
 
 print os.path.expanduser('~')
 
 for root, dirs, files in os.walk(os.path.expanduser('~')):
 	print root, dirs, files
+
+
+2:
+
+print "This is using getsize to see how much every file consumes"
+print "---------------"
+from os.path import join, getsize
+for root, dirs, files in os.walk('~'):
+    print root, "consumes",
+    print sum([getsize(join(root, name)) for name in files]),
+    print "bytes in", len(files), "non-directory files"
+'''
