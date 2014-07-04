450b9f060b0974187ecd0365f0370b7a602e5322
jakesyl <jakesyl@gmail.com>
2014-07-03 21:19:41 -0400

    added new prgm to shit folder

---
+++ theshitfolder/Jake/prgrms/idle.py
@@ -0,0 +1,14 @@
+import os
+
+# If we are not given a path to list, use /tmp
+root='/'
+for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
+    #print '\n', dir_name
+    # Make the subdirectory names stand out with /
+    #sub_dirs = [ '%s/' % n for n in sub_dirs ]
+    # Mix the directory contents together
+    contents = sub_dirs + files
+    contents.sort()
+    # Show the contents
+    #for c in contents:
+        #print '\t%s' % c
