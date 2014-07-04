f88563ec190e6d2fffb62fa0e4d2d6ba8c5be86d
jakesyl <jakesyl@gmail.com>
2014-07-03 21:37:50 -0400

    random shit

--- theshitfolder/Jake/prgrms/idle.py
+++ theshitfolder/Jake/prgrms/idle.py
@@ -5,7 +5,7 @@ root='/'
 for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
     #print '\n', dir_name
     # Make the subdirectory names stand out with /
-    #sub_dirs = [ '%s/' % n for n in sub_dirs ]
+    sub_dirs = [ '%s/' % n for n in sub_dirs ]
     # Mix the directory contents together
     contents = sub_dirs + files
     contents.sort()
