2446b39c3884b2ab382c0f2ec58c8806421be8d4
jakesyl <jakesyl@gmail.com>
2014-07-03 21:20:04 -0400

    basically, this program is now shit

--- theshitfolder/Jake/prgrms/otest.py
+++ theshitfolder/Jake/prgrms/otest.py
@@ -1,5 +1,4 @@
 import os
-
+ 
 for root,dirs, files in os.walk(os.path.expanduser('~')):
-    #values test
-    print "root directory is"+"and in this directory there are " + root + "roots and " + files "files"
+    print dirs
