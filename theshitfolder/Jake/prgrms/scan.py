b705f02e2a5d5f182850181c9845f7bd7782c0aa
jakesyl <jakesyl@gmail.com>
2014-07-13 19:29:07 -0400

    defined function in order to catch

--- theshitfolder/Jake/prgrms/scan.py
+++ theshitfolder/Jake/prgrms/scan.py
@@ -6,7 +6,22 @@ c = conn.cursor()
 
 # If we are not given a path to list, use /tmp
 root='/'
-excludes = '/Applications'
+def dbadd(conn, c, root, dir_name, sub_dirs, files, contents):
+    print f + " is in " + dir_name
+    fpath = dir_name + '/' + f
+    c.execute ("SELECT * FROM clusters WHERE contents = ?", (fpath,))
+    #c.execute('SELECT * FROM clusters WHERE contents = '+ path);
+    clustery = c.fetchone()
+    if clustery is None:
+        cluster = "undefined"
+    else:
+        cluster = clustery[0]
+    cluster = "undefined" #if undefined alex will find it
+    at=os.path.getatime(os.path.join(dir_name, f))#last access time of file
+    c.execute('INSERT INTO scan (fpath, accessDate, cluster) VALUES (?,?,?)', (fpath,at, cluster,))
+
+excludes = ['/Applications']
+
 for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
     #print '\n', dir_name
     # Make the subdirectory names stand out with /
@@ -17,16 +32,16 @@ for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directo
     for f in contents:
         if f in excludes:#check if this directory shouldn't be walked
             continue
-        print f + " is in " + dir_name
-        fpath = dir_name + '/' + f
-        c.execute("SELECT * FROM clusters WHERE contents = ?", (fpath,))
-        #c.execute('SELECT * FROM clusters WHERE contents = '+ path);
-        clustery = c.fetchone()
-        if clustery is None:
-            cluster = "undefined"
-        else:
-            cluster = clustery[0]
-        cluster = "undefined" #if undefined alex will find it
-        at=os.path.getatime(os.path.join(dir_name, f))#last access time of file
-        c.execute('INSERT INTO scan (fpath, accessDate, cluster) VALUES (?,?,?)', (fpath,at, cluster,))#put something that is not retarted here'''
+        try:
+            dbadd(conn, c, root, dir_name, sub_dirs, files, contents)
+        except OSError:
+            print "ERROR ERROR TERROR BE WARRIE"
+            continue
+        
 print "complete"
+
+'''
+output:
+
+ProgrammingError: You must not use 8-bit bytestrings unless you use a text_factory that can interpret 8-bit bytestrings (like text_factory = str). It is highly recommended that you instead just switch your application to Unicode strings.
+'''
