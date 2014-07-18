d10602772e687279e6b29668009447aefefaa6be
jakesyl <jakesyl@gmail.com>
2014-07-18 17:02:11 -0400

    Updated index.py to clone scan.py
    So far it’s runnable

--- src/index.py
+++ src/index.py
@@ -1,72 +1,40 @@
-'''
-Jake Sylvestre
-
-'''
 import sqlite3
 import os
-
+#Table structure for future reference:     c.execute('''CREATE TABLE scan(fpath text, accessDate text)''')
 conn = sqlite3.connect('database.db')#intalizing db
+conn.text_factory = str #what does this do?, no one knows
 c = conn.cursor()
 root='/'
+def dbadd(conn, c, root, dir_name, sub_dirs, files, contents):
+    print f + " is in " + dir_name
+    fpath = dir_name + '/' + f
+    c.execute ("SELECT * FROM scan WHERE fpath = ?", (fpath,))
+    rows = c.fetchall()
+    if (len(rows)!= 0):
+        c.execute ("DELETE * FROM scan WHERE fpath = ?", (fpath,))# Here at cortex we don't do duplicates
 
-for dir_name, sub_dirs, files in os.walk(root): 
+    at=os.path.getatime(os.path.join(dir_name, f))#last access time of file
+    c.execute('INSERT INTO scan (fpath, accessDate) VALUES (?,?)', (fpath,at,))# adds files to sqlite 3 table "scan"
+
+for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
+    #print '\n', dir_name
+    # Make the subdirectory names stand out with / still not 100% sure what this line actually does but, hey it works right?
     sub_dirs = [ '%s/' % n for n in sub_dirs ]
-    contents = files  #originally sub_dirs + files, no longer neccesary
+    # Mix the directory contents together
+    contents = files  #I don't think this is neccesary at all
     contents.sort()
+    #if (dir_name==root): # this isn't working quite right, please come back later!!
+        #sub_dirs.remove('Applications')#same here
+
     for f in contents:
-        print f + " is in " + dir_name #listing contents so I know script is working
-        fpath = dir_name + '/' + f
-        c.execute("SELECT * FROM clusters WHERE contents = ?", (fpath,))
-        clustery = c.fetchone()
-        if clustery is None:
-            cluster = "undefined"
-        else:
-            cluster = clustery[0]
-        cluster = "undefined" #if undefined alex will find it
-        at=os.path.getatime(os.path.join(dir_name, f))#last access time of file
-        c.execute('INSERT INTO scan (fpath, accessDate, cluster) VALUES (?,?,?)', (fpath,at, cluster,))
+        #if f in dir_name:#check if this directory shouldn't be walked
+            #continue
+        try:
+                dbadd(conn, c, root, dir_name, sub_dirs, files, contents)
+        except OSError:
+            print "ERROR ERROR TERROR BE WARRIE"
+            continue
+        
 print "complete"
 
 
-	'''
-Sudocode:
-Using os.walk design a for loop that starts at the root and crawls through every file 
-
-
-
-More Details:
-Dir: ~/cortexstorage/src
-
-other code:
-import os
-
-print os.path.expanduser('~')
-
-for root, dirs, files in os.walk(os.path.expanduser('~')):# for every root in 
-	print root, dirs, files
-	print " "
-
-Explanation for above code:
-root :	Prints out directories only from what you specified
-dirs :	Prints out sub-directories from root. 
-files:  Prints out all files from root and directories
-
-old useless code: # If we are not given a path to list, use /tmp
-if len(sys.argv) == 1:
-    root = '/tmp'
-else:
-    root = sys.argv[1]
-
-for dir_name, sub_dirs, files in os.walk(root):
-    print '\n', dir_name
-    # Make the subdirectory names stand out with /
-    sub_dirs = [ '%s/' % n for n in sub_dirs ]
-    # Mix the directory contents together
-    contents = sub_dirs + files
-    contents.sort()
-    # Show the contents
-    for c in contents:
-        print '\t%s' % c
-
-
-'''
