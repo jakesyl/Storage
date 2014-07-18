7b3d46412c99955fd5f55f2b1e49f07c7f2a5aca
jakesyl <jakesyl@gmail.com>
2014-07-18 19:55:33 -0400

    updated src

--- src/index.py
+++ src/index.py
@@ -19,12 +19,13 @@ def dbadd(conn, c, root, dir_name, sub_dirs, files, contents):
 for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
     #print '\n', dir_name
     # Make the subdirectory names stand out with / still not 100% sure what this line actually does but, hey it works right?
-    sub_dirs = [ '%s/' % n for n in sub_dirs ]
+    #sub_dirs = [ '%s/' % n for n in sub_dirs ]
     # Mix the directory contents together
     contents = files  #I don't think this is neccesary at all
     contents.sort()
-    #if (dir_name==root): # this isn't working quite right, please come back later!!
-        #sub_dirs.remove('Applications')#same here
+    if (dir_name==root): # this isn't working quite right, please come back later!!
+        sub_dirs.remove('Applications')#same here
+        sub_dirs.remove('Library')
 
     for f in contents:
         #if f in dir_name:#check if this directory shouldn't be walked
