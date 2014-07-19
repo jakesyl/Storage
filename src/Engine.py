71ad0a290ba7b8e79a0f5ad132d8cc12cce0cd71
aep000 <alexparson1@gmail.com>
2014-07-18 21:37:30 -0400

    Engine work

--- src/Engine.py
+++ src/Engine.py
@@ -1,4 +1,9 @@
 import ConfigParser
+import sqlite3
+from time import strftime
+from datetime import datetime
+conn = sqlite3.connect('/db/database.db')
+c = conn.cursor()
 def engine(path, time, size):
     config = ConfigParser.RawConfigParser()
     config.read('fTypes.cfg')
@@ -6,5 +11,20 @@ def engine(path, time, size):
     length = len(pathTypes)
     print length
     extention = pathTypes[length-1]
+    extention = (extention, )
     print extention
+    for row in c.execute('SELECT * FROM extentions WHERE ext=?,',extention)
+    if len(row) != 0:
+        date = datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S')
+        date = date.split(" ")
+        date = date[0].split("-")
+        
+        
+
+
+
+
+
+
+
 engine("downloads/shit/tile.txt", "time", 100003)
