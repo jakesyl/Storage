3dc3ab5a57c1e091e9d11b5acd65598bf924075e
jakesyl <jakesyl@gmail.com>
2014-07-18 21:35:23 -0400

    updated paramiko

--- theshitfolder/Jake/prgrms/paramikotest.py
+++ theshitfolder/Jake/prgrms/paramikotest.py
@@ -4,3 +4,10 @@ import interactive
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+client.connect(
+stdin, stdout, stderr = client.exec_commands('insert commands here')
+#so what i'm going to end up doing is cd into the users directory (as in the user who's stuff we are updating, and then upload the stuff there however the fuck that's done
+for line in stdout:
+    #do something
+client.close()
