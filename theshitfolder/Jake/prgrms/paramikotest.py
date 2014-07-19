9edebbebcdd3a2261863050041ee6962043f4f19
jakesyl <jakesyl@gmail.com>
2014-07-19 01:40:19 -0400

    updated ssh test file

--- theshitfolder/Jake/prgrms/paramikotest.py
+++ theshitfolder/Jake/prgrms/paramikotest.py
@@ -1,16 +1,17 @@
-import ecdsa#don't know if this is neccesary
-import Crypto
+#import ecdsa#don't know if this is neccesary
+#import Crypto
 import paramiko
-import interactive# cances are this doesn't actually exsist
+#import interactive #cances are this doesn't actually exsist
 
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
 
-client.connect(
-stdin, stdout, stderr = client.exec_commands('insert commands here')
+client.connect('localhost', 22, 'Jakes-MacBook-Pro-3', 'jake1998')#username may be wrong
+stdin, stdout, stderr = client.exec_commands('ls')
 #so what i'm going to end up doing is cd into the users directory (as in the user who's stuff we are updating, and then upload the stuff there however the fuck that's done
 for line in stdout:
     #do something
+    print line
 client.close()
-need interactive
+#after this works you need to find a way to get name and pass on their computer
