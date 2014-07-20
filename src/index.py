12cce594ba5e5c92afde357ffa751df63a6f9dec
aep000 <alexparson1@gmail.com>
2014-07-19 22:23:41 -0400

    Trying to repair shit DONT TOUCH SCAN OR ENGINE
    JAKE DO NOT TOUCH ANYTHING IN SCAN OR ENGINE

--- src/index.py
+++ src/index.py
@@ -1,11 +1,11 @@
 import sqlite3
 import os
-import engine
+import Engine as engine
 
 #Table structure for future reference:     c.execute('''CREATE TABLE scan(fpath text, accessDate text)''')
 
-conn = sqlite3.connect('database.db')#intalizing db
-conn.text_factory = str #what does this do?, no one knows
+conn = sqlite3.connect('db/database.db')#intalizing db
+conn.text_factory = unicode #what does this do?, no one knows
 c = conn.cursor()
 root='/'
 def dbadd(conn, c, root, dir_name, sub_dirs, files, contents):
@@ -16,12 +16,12 @@ def dbadd(conn, c, root, dir_name, sub_dirs, files, contents):
     rows = c.fetchall()
 
     if (len(rows)!= 0):
-        c.execute ("DELETE * FROM scan WHERE fpath = ?", (fpath,))# Here at cortex we don't do duplicates
+        c.execute ("DELETE FROM scan WHERE fpath = ?", (fpath,))# Here at cortex we don't do duplicates
         
     at=os.path.getatime(os.path.join(dir_name, f))#last access time of file
     size = os.path.getsize(os.path.join(dir_name, f))
     c.execute('INSERT INTO scan (fpath, accessDate) VALUES (?,?)', (fpath,at,))# adds files to sqlite 3 table "scan"
-    engine.engine(fpath,at,size)
+    print engine.engine(fpath,at,size)
     conn.commit()#this might actually be c.commit idk what alex is doing
 
 for dir_name, sub_dirs, files in os.walk(root): #dir_name is the current directory, sub_dirs are subs and files....
