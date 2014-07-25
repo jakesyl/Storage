d99b704979ef77a713123811c9d07ac7585a643c
jakesyl <jakesyl@gmail.com>
2014-07-24 20:26:34 -0400

    .m for app delegate

---
+++ GUI/GUI/clxAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  clxAppDelegate.m
+//  GUI
+//
+//  Created by Jake Sylvestre on 7/24/14.
+//  Copyright (c) 2014 Jake Sylvestre. All rights reserved.
+//
+
+#import "clxAppDelegate.h"
+
+@implementation clxAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
