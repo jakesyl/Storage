311267cc92aaf2d1315f84450dc2aa04a6c56938
aep000 <alexparson1@gmail.com>
2014-07-27 18:34:39 -0400

    gui shit

---
+++ Mac_App/cortex_storage/cortex_storageTests/cortex_storageTests.h
@@ -0,0 +1,13 @@
+//
+//  cortex_storageTests.h
+//  cortex_storageTests
+//
+//  Created by Alex Parson on 7/27/14.
+//  Copyright (c) 2014 Cortex Storage. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cortex_storageTests : SenTestCase
+
+@end
