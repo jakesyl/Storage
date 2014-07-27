a641680950e3c607914a697e4e87db89bf670d32
jakesyl <jakesyl@gmail.com>
2014-07-27 19:07:42 -0400

    finished (but debug) counting.py
    you have a lot of shit to do:
    Traceback (most recent call last):
      File "count.py", line 34, in <module>
        count()# for testing remove later
      File "count.py", line 16, in count
        while (threading.activecount()>9):# if this only applies to this
    file, we'll keep it otherwise i have no idea what to do
    AttributeError: 'module' object has no attribute 'activecount'

--- theshitfolder/Jake/prgrms/count.py
+++ theshitfolder/Jake/prgrms/count.py
@@ -1,3 +1,4 @@
+import time # for sleeping while waiting for threads
 import threading
 import os
 import thread #not sure if threading and thread are even the same thing, but i guess i'll find out
@@ -5,15 +6,18 @@ import thread #not sure if threading and thread are even the same thing, but i g
 #TODO add a shit ton more threads
 #just counts files were indexing
 def count():
-	summation = ()
+	summation = []
+	fincount = 0
 	root=os.path.expanduser('~')#change this before development
 	print "start"
 	#start = timeit.default_timer()
 	dirs = os.listdir(root)
-			for sub_dir in dirs:
-				if (threading.activecount()<9):# if this only applies to this file, we'll keep it otherwise i have no idea what to do
-					thread.start_new_thread(walkthedoge(sub_dir))
-					#array .append the output of the thread i think i'm still not really sure how threading in python works
+	for sub_dir in dirs:
+		while (threading.activecount()>9):# if this only applies to this file, we'll keep it otherwise i have no idea what to do
+				time.sleep(1)
+				thread.start_new_thread(walkthedoge(sub_dir))#start a new thread for every subdir while there are less than 9 threads\
+	for addable in summation:
+		fincount+=addable
 
 
 
@@ -21,14 +25,13 @@ def count():
 		i = i+1#change to i+=1 later
 	return i #returns the count
 #stop = timeit.default_timer()
-count()
 def walkthedoge(sub_dir,summation):
 	i=0
 	for dir_name, sub_dirs, files in os.walk(sub_dir):
-		i = i + 1 #shouldn't this be i+=1
+		i+=1
+	summation.append(i)		
 
-
-
+count()# for testing remove later
 #part of a subproccesss
 
 '''
