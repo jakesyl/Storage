9da7ddac5631f400797676dc7b92d37d3a6ace43
jakesyl <jakesyl@gmail.com>
2014-07-27 18:44:36 -0400

    updated count.py

--- theshitfolder/Jake/prgrms/count.py
+++ theshitfolder/Jake/prgrms/count.py
@@ -1,26 +1,28 @@
+import threading
 import os
 #import timeit
 #TODO add a shit ton more threads
 #just counts files were indexing
 def count():
+	thrad_count = 1 #not using thread.active count because i'm not sure how localized that is...
 	i=1 #fuck naming conventions
 	root=os.path.expanduser('~')#change this before development
 	print "start"
 	#start = timeit.default_timer()
 	for dir_name, sub_dirs, files in os.walk(root):
+		while (dir_name==root):
+			for sub_dirs in dir_name:
+				if (thread_count)
+
 		#print i
 		i = i+1#change to i+=1 later
 	return i #returns the count
 #stop = timeit.default_timer()
-
-'''
-Note this might already be fixed (i'm rerunning)
-
-TODO fix this error:
-Traceback (most recent call last):
-  File "count.py", line 12, in <module>
-    stop = timeit.default_timer()
-TypeError: cannot concatenate 'str' and 'int' objects
+count()
+def 
 
 
 '''
+sudocode:
+*find out which files are in ~
+*start a new thread for each 
