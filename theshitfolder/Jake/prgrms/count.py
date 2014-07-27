0f3459f04eb1f806f530960e52d443a7df536e02
jakesyl <jakesyl@gmail.com>
2014-07-27 18:57:39 -0400

    working on count.py

--- theshitfolder/Jake/prgrms/count.py
+++ theshitfolder/Jake/prgrms/count.py
@@ -1,28 +1,40 @@
 import threading
 import os
+import thread #not sure if threading and thread are even the same thing, but i guess i'll find out
 #import timeit
 #TODO add a shit ton more threads
 #just counts files were indexing
 def count():
-	thrad_count = 1 #not using thread.active count because i'm not sure how localized that is...
-	i=1 #fuck naming conventions
+	summation = ()
 	root=os.path.expanduser('~')#change this before development
 	print "start"
 	#start = timeit.default_timer()
-	for dir_name, sub_dirs, files in os.walk(root):
-		while (dir_name==root):
-			for sub_dirs in dir_name:
-				if (thread_count)
+	dirs = os.listdir(root)
+			for sub_dir in dirs:
+				if (threading.activecount()<9):# if this only applies to this file, we'll keep it otherwise i have no idea what to do
+					thread.start_new_thread(walkthedoge(sub_dir))
+					#array .append the output of the thread i think i'm still not really sure how threading in python works
+
+
 
 		#print i
 		i = i+1#change to i+=1 later
 	return i #returns the count
 #stop = timeit.default_timer()
 count()
-def 
+def walkthedoge(sub_dir,summation):
+	i=0
+	for dir_name, sub_dirs, files in os.walk(sub_dir):
+		i = i + 1 #shouldn't this be i+=1
 
+
+
+#part of a subproccesss
 
 '''
 sudocode:
-*find out which files are in ~
+*find out which directories are in ~
 *start a new thread for each 
+*os.walk all of them
+handle files using isdir
+'''
