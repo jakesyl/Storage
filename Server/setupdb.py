d71d0569cf6ab4a5de2e1297fcf96a4cc0b089d7
jakesyl <jakesyl@gmail.com>
2014-07-29 18:37:44 -0400

    added script to setup db
    to test psycopg2 module

---
+++ Server/setupdb.py
@@ -0,0 +1,26 @@
+import psycopg2 as pgsql #for postgresql interaction
+import logging
+
+#logging setup
+logging.basicConfig(level=logging.INFO)#adjust level to see different levels of stuff
+logger = logging.getLogger(__name__)
+
+try:
+	conn = pgsql.connect("dbname='users' user='postgres' host='localhost' password='jake1998'")
+except:
+    logger.info("I am unable to connect to the database")
+
+c = conn.cursor()#We have to define a connection cursor in order to execute commands
+
+
+try:
+	('''SELECT * FROM  users''')
+except:
+	c.execute('''CREATE TABLE users(username text, tableadress int,userpath)''')
+	logger.info("Table filemap doesn't exsist creating it")
+
+try:
+	('''SELECT * FROM  newusers''')
+except:
+	c.execute('''CREATE TABLE newusers(username text)''')
+	logger.info("Table newusers doesn't exsist creating it")
