dda682cd3f2453de0b3be41cc64efe7157f627d0
jakesyl <jakesyl@gmail.com>
2014-07-29 15:30:28 -0400

    added start file

---
+++ theshitfolder/Jake/central_start/start.py
@@ -0,0 +1,21 @@
+import os
+import logging
+import index
+import configSetup as config
+import Dbsetup as makedb
+import index
+
+#Setup Logging 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+contents = os.listdir(os.getcwd())#gets the contents of the current directory
+
+if 'config.cfg' not in contents:#Makes config file if it's not there
+	logger.info("No config.cfg exsist, making one")
+	config.configSetup()
+if 'database.db' not in contents:#build a database if it's not there	TODO I don't know if these things should be in ()
+	logger.info("No database.db exsist, making one")
+	makedb.makedb()
+
+index.index()
