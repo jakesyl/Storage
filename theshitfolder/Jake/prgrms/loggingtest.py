f1240e93d2ea13ff696e1b0c172c88033a2f02f7
jakesyl <jakesyl@gmail.com>
2014-07-29 14:50:32 -0400

    updated logging test (it works!)

--- theshitfolder/Jake/prgrms/loggingtest.py
+++ theshitfolder/Jake/prgrms/loggingtest.py
@@ -1,13 +1,12 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.debug)
-logger2 = logging.getlogger(name)
+
 logger.info('Start reading database')
 # read database here
 
 records = {'john': 55, 'tom': 66}
-logger2.debug('Records: %s', records)
+logger.debug('Records: %s', records)
 logger.info('Updating records ...')
 # update records here
 
