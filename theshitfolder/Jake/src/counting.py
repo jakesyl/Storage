5e108d136ab0504e94c47843d96aee496df1380d
jakesyl <jakesyl@gmail.com>
2014-08-01 21:05:55 -0400

    updated counting.py

--- theshitfolder/Jake/src/counting.py
+++ theshitfolder/Jake/src/counting.py
@@ -6,6 +6,7 @@ import logging
 #TODO add a shit ton more threads
 #just counts files were indexing
 def counting():
+	cnt_and_rd = []
 	#Log Here
 	logging.basicConfig(level=logging.INFO)
 	logger = logging.getLogger(__name__)
@@ -19,8 +20,12 @@ def counting():
 		try:
 			i = i+1#change to i+=1 later
 			logger.info("Counted to file " + str(i))
+		for dir_name in subdirs:
+			if (dir_name[0]==".")
+				cnt_and_rd.append(dir_name)
 		except BaseException:#Nothing could go wrong, right?
 			logger.debug("An Error Occured, Continuing")
-			continue 
-	return i #returns the count
+			continue
+	cnt_and_rd.insert(1,i)
+	return cnt_and_rdi #returns the count
 	#stop = timeit.default_timer()
