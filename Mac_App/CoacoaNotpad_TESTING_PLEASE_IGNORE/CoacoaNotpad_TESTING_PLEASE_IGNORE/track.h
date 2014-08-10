7f42912e5dba2d9ee4c249ac9e4186aa924db057
aep000 <alexparson1@gmail.com>
2014-08-10 00:21:58 -0400

    Testing

---
+++ Mac_App/CoacoaNotpad_TESTING_PLEASE_IGNORE/CoacoaNotpad_TESTING_PLEASE_IGNORE/track.h
@@ -0,0 +1,13 @@
+//
+//  track.h
+//  CoacoaNotpad_TESTING_PLEASE_IGNORE
+//
+//  Created by Alex Parson on 8/9/14.
+//  Copyright (c) 2014 Cortex Storage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface track : NSObject
+@property (assign) float volume;
+@end
