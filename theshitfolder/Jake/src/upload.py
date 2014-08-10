7aa793fc68158fadc7ebfe88a38d8556373dd4d6
jakesyl <jakesyl@gmail.com>
2014-08-10 18:21:11 -0400

    added upload.py

--- theshitfolder/Jake/src/upload.py
+++ theshitfolder/Jake/src/upload.py
@@ -10,13 +10,16 @@ def upload():
 	    client.load_system_host_keys()
 	    client.set_missing_host_key_policy(paramiko.WarningPolicy)
 
-	    client.connect('localhost', 22, 'root', 'jake1998')#username may be wrong
+	    client.connect('2601:c:3480:379:aa20:66ff:fe35:3e73', 22, 'jcortex', 'jake1998')#username may be wrong
 
 	    #so what i'm going to end up doing is cd into the users directory (as in the user who's stuff we are updating, and then upload the stuff there however the fuck that's done
 	    stdin, stdout, stderr = client.exec_command('ls')
 	    print stdout.read(),
+	except Exception, e: # will catch later
+		print str(e)
 	finally:
 	    client.close()
 
 
 #after this works you need to find a way to get name and pass on their computer
+upload()
