5108fec991b74c640ab3b68d39fc6a769123fade
aep000 <alexparson1@gmail.com>
2014-08-10 18:19:04 -0400

    shit

---
+++ Mac_App/Cortex Storage/Cortex StorageTests/Cortex_StorageTests.h
@@ -0,0 +1,13 @@
+//
+//  Cortex_StorageTests.h
+//  Cortex StorageTests
+//
+//  Created by Alex Parson on 8/10/14.
+//  Copyright (c) 2014 Cortex Storage. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Cortex_StorageTests : SenTestCase
+
+@end
