e56a4d07e2e7b4287e08878ed946a2ee5aaa1554
jakesyl <jakesyl@gmail.com>
2014-08-14 12:19:24 -0400

    sftp fix

--- theshitfolder/Jake/src/sftp.py
+++ theshitfolder/Jake/src/sftp.py
@@ -4,14 +4,10 @@ Cortex Storage
 Script to upload files through use of sftp
 '''
 import pysftp as ftp#used for server interaction through sftp
-def uploadfile(ahost, ausername, apassword,remotepath,localpath):
-	try:
-		s = ftp.Connection(host = ahost,username= ausername, password = apassword)
-		s.put(localpath,remotepath)
+def uploadfile(ahost, ausername, apassword,aremotepath,localpath):
+	s = ftp.Connection(host = ahost,username= ausername, password = apassword)
+	ftp.cd(aremotepath)
+	s.put(localpath,preserve_mtime=True)
+	s.close
 
-		s.close
-
-	except Exception, e: # will catch later
-		print str(e)
-
-uploadfile('2601:c:3480:379:c523:346c:2c10:59aa', 'jcortex', 'jake1998', '/home/jcortex', '/Users/Jake_Sylvestre/Documents/x.py')
+uploadfile('2601:c:3480:379:c523:346c:2c10:59aa', 'jcortex', 'jake1998', '/home/jcortex/cortexstorage', '/Users/Jake_Sylvestre/Documents/x.py')
