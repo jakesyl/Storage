831718779446670c201a2ab6ec89e9240e0ab526
jakesyl <jakesyl@gmail.com>
2014-08-27 20:22:34 -0400

    fixed #~ and beggining comment

--- theshitfolder/Jake/src/counting.py
+++ theshitfolder/Jake/src/counting.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-
 '''
 Cortex Storage
 Determines wether or not a file should be uploaded by means of time
 '''
+
 import os
 import logging
 #import timeit
@@ -14,24 +14,23 @@ def counting():
 	logging.basicConfig(level=logging.INFO)
 	logger = logging.getLogger(__name__)
 	
-	root=os.path.expanduser('~')#change this before development
+	root = os.path.expanduser('~')#change this before development
 	#print "start"
-	i=1
+	counting = 1
 	#start = timeit.default_timer()
 	for dir_name, sub_dirs, files in os.walk(root):
-		#print i
 		try:
-			i = i+1#change to i+=1 later
-			logger.info("Counted to file " + str(i))
+			counting += 1
+			logger.info("Counted to file " + str(counting))
 			for adir in sub_dirs:
 				if (adir[0]=="."):
 					logger.info("We found a directory to remove!")
-					fpath = adir + '/' + f
+					fpath = adir + '/' + files
 					cnt_and_rd.append(fpath)
 		except BaseException:#Nothing could go wrong, right?
 			logger.debug("An Error Occured, Continuing")
 			continue
-	cnt_and_rd.insert(1,i)
+	cnt_and_rd.insert(1, counting)
 	return cnt_and_rd #returns the count
 	#stop = timeit.default_timer()
 #counting()#for testing
