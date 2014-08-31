e60f294d1aeb9f569b29ee091e4902487c84c31a
jakesyl <jakesyl@gmail.com>
2014-08-30 21:47:25 -0400

    added webdav

--- Website/login_check.php
+++ Website/login_check.php
@@ -6,30 +6,30 @@ $password="jake1998"; // password
 $db_name="test"; // DB name 
 $tbl_name="members"; // Table name 
 
-$conn_string = "host= " + localhost + "dbname= " + db_name + " user= " + username + " password = " + password;
+$conn_string = "host= " + $localhost + "dbname= " + $db_name + " user= " + $username + " password = " + $password;
 
 // Connect to server and select databse.
 pg_connect($conn_string) or die "can't connect";
 // username and password sent from form 
-$myusername=$_POST['myusername']; 
+$username=$_POST['username']; 
 $mypassword=$_POST['mypassword']; 
 
 // To protect MySQL injection (more detail about MySQL injection)
-$myusername = stripslashes($myusername);
+$username = stripslashes($username);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
+$username = mysql_real_escape_string($username);
 $mypassword = mysql_real_escape_string($mypassword);
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$mypassword'";
 $result=mysql_query($sql);
 
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 
-// If result matched $myusername and $mypassword, table row must be 1 row
+// If result matched $username and $mypassword, table row must be 1 row
 if($count==1){
 
-// Register $myusername, $mypassword and redirect to file "login_success.php"
-session_register("myusername");
+// Register $username, $mypassword and redirect to file "login_success.php"
+session_register("username");
 session_register("mypassword"); 
 header("location:success.php");
 }
