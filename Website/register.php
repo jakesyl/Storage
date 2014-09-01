d64e0495ec8ae6082b97ab1c4fb942a6d2564df0
jakesyl <jakesyl@gmail.com>
2014-08-31 20:08:40 -0400

    erlich kidnapped a kid again

---
+++ Website/register.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>
+<strong> Cortex Storage </strong>
+</title>
+</head>
+<body>
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form name="form1" method="post" action="register_check.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3"><strong>Register</strong></td>
+</tr>
+<tr>
+<td width="78">Full Name</td>
+<td width="6">:</td>
+<td width="294"><input name="Name" type="text" id="Name"></td>
+</tr>
+<tr>
+<td>Address</td>-
+<td>:</td>
+<td><input name="Address" type="text" id="Address"></td>
+</tr>
+<tr>
+<td width="78">City</td>
+<td width="6">:</td>
+<td width="294"><input name="City" type="text" id="City"></td>
+</tr>
+<tr>
+<td width="78">Zip</td>
+<td width="6">:</td>
+<td width="294"><input name="Zip" type="text" id="Zip"></td>
+</tr>
+<tr>
+<td width="78">Full Name</td>
+<td width="6">:</td>
+<td width="294"><input name="Name" type="text" id="Name"></td>
+</tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
+</body>
+</html>
---
+++ Website/tablestructure.md
@@ -0,0 +1,9 @@
+CREATE TABLE members
+(
+ID int,
+Name varchar(255),
+Address varchar(255),
+City varchar(255),
+Zip varchar(255),
+Password varchar(255)
+);
